88ca6ed4048a8a74c88e47ba0ab387ff09074c1c
HotelBudapest <arianislam.010@gmail.com>
2024-04-08 23:51:56 +1000

    background, foreground  and trees added

--- src/main/java/Tanks/App.java
+++ src/main/java/Tanks/App.java
@@ -26,12 +26,17 @@ public class App extends PApplet {
     public static int HEIGHT = 640; //BOARD_HEIGHT*CELLSIZE+TOPBAR;
     public static final int BOARD_WIDTH = WIDTH/CELLSIZE;
     public static final int BOARD_HEIGHT = 20;
+    public int[][] board = new int[32][32];
+    String trees;
 
     public static final int INITIAL_PARACHUTES = 1;
 
     public static final int FPS = 30;
 
     public String configPath;
+    public String backgroundImage;
+    String foregroundColour;
+    String layout;
 
     public static Random random = new Random();
 	
@@ -55,6 +60,53 @@ public class App extends PApplet {
 	@Override
     public void setup() {
         frameRate(FPS);
+        JSONObject json = loadJSONObject(configPath);
+
+        JSONArray level = json.getJSONArray("levels");
+        JSONObject current = level.getJSONObject(0);
+        layout = current.getString("layout");
+        backgroundImage = current.getString("background");
+        String foregroundColour = current.getString("foreground-colour");
+        if(current.hasKey("trees")){
+            trees = current.getString("trees");
+        }
+        else{
+            trees = null;
+        }
+
+        try{ 
+			BufferedReader reader = new BufferedReader(new FileReader(layout));
+			
+            int i =0; 
+            while (true){
+                String line = reader.readLine();
+                if (line == null){
+                    break;
+                }
+                String[] line_sep = line.split("");
+                for (int j = 0; j < line_sep.length; j++){
+                    if (line_sep[j].equals("X")){
+                        board[i][j] = 1;
+                    }
+                    else if (line_sep[j].equals("T")){
+                        board[i][j] = 2;
+                    }
+                }
+                i++;
+            }
+
+            for (int k = 0; k < board.length; k++){
+                System.out.println(Arrays.toString(board[k]));
+            }
+            reader.close();
+            }
+            catch(FileNotFoundException e){
+                e.printStackTrace();
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        
 
     }
 
@@ -107,6 +159,24 @@ public class App extends PApplet {
         //----------------------------------
 
         //TODO: Check user action
+
+        background(255); // Optional: Clears the background with white color
+        image(loadImage(backgroundImage), 0, 0, width, height);
+
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[i][j] == 1){
+                    for (int k = i; k <board.length;k++){
+                        noStroke();
+                        fill(255,255,255);
+                        rect(j*CELLSIZE, k*CELLSIZE, CELLSIZE, CELLSIZE);
+                    }
+                }
+                else if (board[i][j] == 2){
+                    image(loadImage(trees), j*CELLSIZE, i*CELLSIZE, 30, 30);
+                }
+            }
+        }
     }
 
 
